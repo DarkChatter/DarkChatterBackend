24e647e7b551ae29f247202f28faabea01c79b87
Ryan Hayes <rhayes2@myseneca.ca>
2018-11-06 15:27:05 -0500

    Added comment

--- ScappyTest.py
+++ ScappyTest.py
@@ -9,8 +9,8 @@ from scapy.all import *
 
 def build_packet(x):
     target="www.google.com"
-    ip=IP(dst=target)
-    print(Net('www.google.com'))
+    ip=IP(dst=target)                  # Packets will be filled with default info 
+    print(Net('www.google.com'))       # If you dont specify 
     sendp(ip , loop=1,inter=0.2);
     
     #sendp("I'm travelling on Ethernet", loop=1, inter=0.2)
