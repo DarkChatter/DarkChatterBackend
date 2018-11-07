ab90b2c7bd57c4b224d143ca1a70da638c8699ae
rhayes2 <hayes.ryan83@gmail.com>
2018-11-07 00:45:15 -0500

    Sample Update

--- ScappyTest.py
+++ ScappyTest.py
@@ -8,11 +8,18 @@ from scapy.all import *
 
 
 def build_packet(x):
-    target="www.google.com"
-    ip=IP(dst=target)                  # Packets will be filled with default info 
-    print(Net('www.google.com'))       # If you dont specify 
-    sendp(ip , loop=1,inter=0.2);
-    
+    ip=IP("HelloWOrld")
+    #sendp(ip , loop=1,inter=0.2,iface="wlp2s0");
+    sendp(RadioTap()/
+          Dot11(addr1="ff:ff:ff:ff:ff:ff",
+                addr2="00:01:02:03:04:05",
+                addr3="00:01:02:03:04:05")/
+          Dot11Beacon(cap="ESS", timestamp=1)/
+          Dot11Elt(ID="SSID", info='hellonameisRyan')/
+          Dot11Elt(ID="Rates", info='\x82\x84\x0b\x16')/
+          Dot11Elt(ID="DSset", info="\x03")/
+          Dot11Elt(ID="TIM", info="\x00\x01\x00\x00"),
+          iface="wlp2s0mon", loop=1)
     #sendp("I'm travelling on Ethernet", loop=1, inter=0.2)
     
   
